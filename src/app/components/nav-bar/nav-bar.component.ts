61b6593998718c92d9de39881394dd32545a8f8e
Josué138 <c.oviedo@dappertechnologies.com>
2025-05-27 15:58:27 -0500

    Update navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+
+  public sidebarVisible: boolean = false;
+  private scrollListener: boolean = true;
+  public scrollFlag: string = '';
+  public url: string = '';
+  public paddingScrollHeader = '';
+
+  public routers = [
+    {
+      nameEn: 'HOME',
+      nameEs: 'INICIO',
+      route: '/home',
+      goTo: 'nav'
+    },
+  ]
+
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: any,
+  ) { }
+
+  ngOnInit(): void {
+    this.scrollListener = true;
+    this.renderer.listen('window', 'scroll', (e) => {
+      if (this.scrollListener) {
+        this.scrollFlag =
+          e.target.scrollingElement.scrollTop > 75 ? 'scroll' : '';
+        this.paddingScrollHeader = '0px';
+      }
+    });
+  }
+
+  goTo(path: any, event?: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        let element = document.querySelector('#' + path) as HTMLElement;
+        let topOfElement = element.offsetTop - 100;
+        window.scroll({ top: topOfElement, behavior: 'smooth' });
+      }, 200);
+    }
+  }
+
+  search(value: string) {
+    this.router.navigate(['/properties'], {
+      queryParams: {
+        operation: value,
+      }
+    });
+  }
 
 }
