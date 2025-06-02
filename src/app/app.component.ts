59c9ae2e219762164d8653d45c912c62aaf9fb82
Angel Rosaldo <angelmanuelrosaldo72@gmail.com>
2025-06-02 01:07:23 -0500

    Second banner finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 import SwiperCore, { Autoplay, Pagination, Navigation, EffectFade } from 'swiper';
+import { Option } from './components/banner-sec/options.component';
 
 import * as AOS from 'aos';
 SwiperCore.use([Autoplay, Pagination, Navigation, EffectFade]);
@@ -13,7 +14,7 @@ SwiperCore.use([Autoplay, Pagination, Navigation, EffectFade]);
 })
 export class AppComponent {
 
-  title = 'tequila-front';
+  title = 'pueblos-magicos-app';
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -38,6 +39,39 @@ export class AppComponent {
     }, 100);
   }
 
+  onOptionSelected(option: Option): void {
+    console.log('Opción seleccionada:', option);
+
+    // Aquí puedes manejar la lógica cuando se selecciona una opción
+    // Por ejemplo, navegar a una ruta específica, filtrar contenido, etc.
+
+    switch(option.id) {
+      case 'vive-magia':
+        console.log('Navegando a Vive su magia');
+        this.router.navigate(['/vive-magia']);
+        break;
+      case 'atractivos-actividades':
+        console.log('Navegando a Atractivos actividades');
+        this.router.navigate(['/atractivos-actividades']);
+        break;
+      case 'festividades':
+        console.log('Navegando a Festividades');
+        this.router.navigate(['/festividades']);
+        break;
+      case 'hoteles':
+        console.log('Navegando a Hoteles');
+        this.router.navigate(['/hoteles']);
+        break;
+      case 'restaurante':
+        console.log('Navegando a Restaurante');
+        this.router.navigate(['/restaurante']);
+        break;
+      case 'tours':
+        console.log('Navegando a Tours');
+        this.router.navigate(['/tours']);
+        break;
+      default:
+        console.log('Opción no reconocida');
+    }
+  }
 }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { OptionsComponent } from './components/banner-sec/options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
