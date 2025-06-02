9ba9d3b32879834b8c3af98826045cdd25fd33cc
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2025-06-02 09:31:09 -0600

    complete section

--- src/app/components/attractions/attractions.component.ts
+++ src/app/components/attractions/attractions.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './attractions.component.scss'
 })
 export class AttractionsComponent {
-
+  public url = 'assets/images/Attractions/'
+  public attractions = [`${this.url}1.webp`,`${this.url}2.jpeg`,`${this.url}3.jpeg`,`${this.url}4.avif`,`${this.url}5.jpg`];
 }
