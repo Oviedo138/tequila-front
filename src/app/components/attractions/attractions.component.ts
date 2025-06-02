69bbf0e04a0bec05f869b4c7b9cc5c37a87029a2
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2025-06-02 09:49:46 -0600

    last image

--- src/app/components/attractions/attractions.component.ts
+++ src/app/components/attractions/attractions.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-attractions',
   templateUrl: './attractions.component.html',
-  styleUrl: './attractions.component.scss'
+  styleUrl: './attractions.component.scss',
 })
 export class AttractionsComponent {
-  public url = 'assets/images/Attractions/'
-  public attractions = [`${this.url}1.webp`,`${this.url}2.jpeg`,`${this.url}3.jpeg`,`${this.url}4.avif`,`${this.url}5.jpg`];
+  public url = 'assets/images/Attractions/';
+  public attractions = [
+    `${this.url}1.webp`,
+    `${this.url}2.jpeg`,
+    `${this.url}3.jpeg`,
+    `${this.url}4.avif`,
+    `${this.url}5.jpg`,
+    `${this.url}6.jpg`,
+  ];
 }
